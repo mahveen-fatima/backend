afc68ffd47275b5e234c4978fbef214f7e1c7d99
mahveen-fatima <mahveenfatima2224@gmail.com>
2024-07-05 07:57:39 +0530

    changes in app

--- src/app.js
+++ src/app.js
@@ -4,14 +4,26 @@ import cookieParser from "cookie-parser"
 
 const app = express()
 
+// it configures after importing express( after making app )
+// 1. configure cors by using ( use ) it is used for configuring and set the middlewares.
+
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
+    credentials: true 
 }))
 
+// 2. settings for getting data and configuring it with express
 app.use(express.json({limit: "16kb"}))
+// getting the data after filling the form
+
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
+// getting the data from url
+
 app.use(express.static("public"))
+// for storing pdfs, files, folders, images in folder public can access anyone
+
 app.use(cookieParser())
+// to access and set the cookies to the browser of user
+// server can read and remove the cookie
 
 export { app }
